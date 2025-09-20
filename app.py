bc1228b3ce8e124dbf1bd65af3366fbd37e1e8dc
kirti734 <145695522+kirti734@users.noreply.github.com>
2025-09-20 15:31:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import difflib
 import hashlib
 import traceback
 
-GOOGLE_API_KEY = "GEMINI_API_KEY"
+GOOGLE_API_KEY = GEMINI_API_KEY
 
 genai.configure(api_key=GOOGLE_API_KEY)
 model = genai.GenerativeModel("gemini-2.5-flash")
@@ -384,4 +384,5 @@ app.secret_key = 'your-secret-key-here'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
+
 
